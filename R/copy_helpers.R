a8ac6adb8d98a1a3fbd37721c88150d11220f9fa
Witold Wolski <wewolski@gmail.com>
2024-03-20 11:04:37 +0100

    added bibliography file.

--- R/copy_helpers.R
+++ R/copy_helpers.R
@@ -9,38 +9,18 @@ copy_SAINTe_doc <- function(workdir = getwd()){
   prolfqua:::.scriptCopyHelperVec(runscripts, workdir = workdir)
 }
 
-.run_markdown_with_params <-
-  function(params,
-           markdown_path,
-           dest_path,
-           dest_file_name,
-           workdir = tempdir(),
-           packagename = "prolfqua",
-           format = "pdf") {
-    res <- .scriptCopyHelperVec(markdown_path,
-                                workdir = workdir,
-                                packagename = packagename)
-    dist_file_path <-
-      file.path(dest_path, paste0(dest_file_name, ".", format))
-    if (is.null(res)) {
-      return(NULL)
-    }
-    rmarkdown::render(
-      res[1],
-      output_format = if (format == "pdf") {
-        bookdown::pdf_document2()
-      } else{
-        bookdown::html_document2()
-      },
-      params = params,
-      envir = new.env()
-    )
 
-    pdf_doc <- paste0(tools::file_path_sans_ext(res[1]), ".", format)
-    message("XXXX--------------------------------------XXXX")
-    if (pdf_doc != dist_file_path) {
-      message("from " , pdf_doc, " to ", dist_file_path)
-      file.copy(pdf_doc, dist_file_path, overwrite = TRUE)
-    }
-    return(dist_file_path)
-  }
+#' copy Markdown and runscript for DIANN diann-output.tsv
+#' @param workdir directory where to copy file - default is current working directory.
+#' @export
+#'
+copy_SAINT_express <- function(workdir = getwd(), run_script = FALSE) {
+  runscripts <- c(
+    "application/bibliography.bib",
+    "application/SE2/SaintExpressReportMsFragger.Rmd",
+    if (run_script) {"application/SE2/CreateSaintExpress_Report.R"},
+    if (run_script) {"application/SE2_DIANN/DIANN_SE.R"}
+  )
+  prolfqua:::.scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
+}
+
